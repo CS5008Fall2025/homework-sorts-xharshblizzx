14837fb77a7e453350748e4ab9e6734abcd27502
xharshblizzx <harshblizz15@gmail.com>
2025-09-30 16:58:26 -0400

    Update 1.0 sort_helper.h

--- src/sort_helper.h
+++ src/sort_helper.h
@@ -7,7 +7,12 @@
 // Input: The 'address of' an index into an array for positions in an array.
 void swap(int *a, int *b)
 {
-    // TODO: Swap two integers in an array.
+    // Store value at address a in temp variable
+    int temp = *a;
+    // Put value from b into a
+    *a = *b;
+    // Put the original value from a into b
+    *b = temp;
 }
 
 // Input: A pointer to an array (i.e. the array itself points to the first index)
